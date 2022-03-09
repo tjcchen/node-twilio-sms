b0d648bbca1f78a1b526ba05e0e4f5f0fade45fd
Andy Chen <chenyangpoppin@hotmail.com>
2022-03-09 22:14:34 +0800

    feat: update phone number

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
+     from: process.env.MY_PHONE_NUMBER,
+     to: '+8613821576650'
    })
   .then(message => console.log(message.sid));
