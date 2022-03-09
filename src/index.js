ea0284ac28ba65e6503d599e0ed3110eb0b34f32
Andy Chen <chenyangpoppin@hotmail.com>
2022-03-09 22:38:09 +0800

    feat: load .env variables with dotenv module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// Enable dotenv to load environment variables
+require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+console.log('accountSid', accountSid);
+console.log('authToken', authToken);
 
 const client = require('twilio')(accountSid, authToken);
 
